fb758e759e7d18e1a21b52715f70d23d5b146ef5
Ashish Basnet <ashishbasnet468@gmail.com>
2024-05-07 11:39:48 +0100

    styles changes

--- src/screens/auth/login/LoginScreen.tsx
+++ src/screens/auth/login/LoginScreen.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 
 function LoginScreen() {
   return (
-    <View>
+    <View style={styles.conatiner}>
         <Text>LoginScreen</Text>
+        
     </View>
   )
 }
 
 export default LoginScreen; 
 
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1, 
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#3843FF"
+  }
+})
