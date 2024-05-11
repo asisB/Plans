22d37c97d0d9101f39999d9eafa2f6cad4ad97d2
Ashish Basnet <ashishbasnet468@gmail.com>
2024-05-11 13:53:12 +0100

    update

--- src/screens/auth/splash/SplashScreen.tsx
+++ src/screens/auth/splash/SplashScreen.tsx
@@ -1,13 +1,15 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React, {useEffect} from 'react'
-import { useNavigation } from '@react-navigation/native';
 
 export default function SplashScreen() {
   return (
-    <View style={{flex: 1, justifyContent: "center", alignItems: 'center', backgroundColor: "white"}}>
+    <View style={{flex: 1, backgroundColor: "white"}}>
         <Image style={{width: 200, height: 200}} source={require('../../../assets/images/background.png')}/>
         <View>
-        <Text style={styles.text}>Welcome {"\n"} To Your {"\n"} Personal Planner</Text>
+          <View style={{justifyContent: "center", alignItems: 'center'}}>
+          <Text style={styles.text}>Welcome {"\n"} To Your {"\n"} Personal Planner</Text>
+          </View>
+        
         </View>
         
     </View>
